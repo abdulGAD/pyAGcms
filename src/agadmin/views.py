9e513c8bdf10ad57d62a0afdb4a705c68e2f073b
Abdul Gadir <gadir@rol.republika.co.id>
2024-08-22 11:32:42 +0700

    starter django cms

---
+++ src/agadmin/views.py
@@ -0,0 +1,41 @@
+from django.http  import HttpResponse
+from django.shortcuts import render
+
+
+from visits.models import pageVisit
+
+def home(request, *args, **kwargs):
+    pageVisit.objects.create(path=request.path)
+    
+    path =  request.path
+    qs    = pageVisit.objects.all()
+    page_qs    = pageVisit.objects.filter(path=request.path)
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title,
+        "querySet": page_qs.count,
+        "total": qs.count,
+        "percent": (page_qs.count() * 100.0) / qs.count() ,
+        
+    }
+    html_template = "home.html"
+    return render(request,html_template,my_context)
+
+def old_home(request, *args, **kwargs):
+  
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_ = """
+    <!DOCTYPE html>
+<html>
+
+<body>
+    <h1>{page_title} anything?</h1>
+</body>
+</html>    
+""".format(**my_context) # page_title=my_title
+    # html_file_path = this_dir / "home.html"
+    # html_ = html_file_path.read_text()
+    return HttpResponse(html_)
